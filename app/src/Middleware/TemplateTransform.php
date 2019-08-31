433e9c43b141e6b605281594cd202a33df177f85
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-08-31 23:03:47 +0400

    fix(middleware): Transform middleware return value for break resolver

--- app/src/Middleware/TemplateTransform.php
+++ app/src/Middleware/TemplateTransform.php
@@ -10,6 +10,7 @@ class TemplateTransform extends Middleware
     {
         $this->response->setBody('<!-- microsoft page generator v 2.0-->'.PHP_EOL);
         ($this->next)();
-        return $this->response->setBody(PHP_EOL.'<!-- page completed by microsoft page generator-->');
+        $this->response->setBody(PHP_EOL.'<!-- page completed by microsoft page generator-->');
+        return true;
     }
 }
