08b47f68c197d2a41895f15e8617c9be90f322e5
Alexey Dobrynin <ag.dobrynin@gmail.com>
2019-08-31 23:12:24 +0400

    feat(routes): Test json route

--- config/routes.php
+++ config/routes.php
@@ -8,6 +8,8 @@ $app->get('/task/show/(?<id>\d+)', App\Controllers\Task\Show::class)->setName('t
 $app->get('/login', App\Controllers\Auth::class . '@show')->setName('login');
 $app->post('/login', App\Controllers\Auth::class . '@login');
 $app->get('/logout', App\Controllers\Auth::class . '@logout')->setName('logout');
-//
+$app->get('/json/(?<word>\w+)', static function (string $word) use ($app) {
+    $app->response->setJson(['Hello' => $word?:'World']);
+});
 // Защита POST формы через Csrf - по токену
 $app->middleware(App\Middleware\CsrfMiddleware::class);
