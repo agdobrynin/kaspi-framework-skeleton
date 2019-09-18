019cba5fcb82890cd6fd93326eb921f70c3f819c
agdobrynin <ag.dobrynin@gmail.com>
2019-09-18 16:45:01 +0400

    fix(config): new comment for view extention

--- config/dependencies.php
+++ config/dependencies.php
@@ -15,7 +15,7 @@ $app->getContainer()->set(View::class, static function () use ($container): View
             return $router->getRoutePatternByName($pathName, $arg) ?: '';
         });
         // Пример добавления расширения для View
-        // URI вызов в шаблоне $this->addExtension('URI') вернет текущий URI из объекта REQUEST
+        // URI вызов в шаблоне $this->getExt('URI') вернет текущий URI из объекта REQUEST
         /** @var Request $request */
         $request = $container->{Request::class};
         $view->addExtension('URI', static function () use ($request) {
